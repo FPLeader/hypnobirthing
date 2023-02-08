07bc77a1eeb26f2a1b0b776a5ddd7f8b1478e5ef
fpleader <alancameronboyd@gmail.com>
2023-02-07 19:57:57 -0800

    update

--- components/ButtonComponents/SearchButton.tsx
+++ components/ButtonComponents/SearchButton.tsx
@@ -3,11 +3,11 @@ import React from 'react'
 export default function SearchButton() {
     return (
         <div className='flex items-center w-full'>
-            <div className="relative w-full relative">
-                <div className="absolute top-2.5 right-2 flex items-center pl-3 pointer-events-none">
-                    <svg aria-hidden="true" className="w-5 h-5 text-deviders" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clipRule="evenodd"></path></svg>
+            <div className='relative w-full relative'>
+                <div className='absolute top-2.5 right-2 flex items-center pl-3 pointer-events-none'>
+                    <svg aria-hidden='true' className='w-5 h-5 text-deviders' fill='currentColor' viewBox='0 0 20 20' xmlns='http://www.w3.org/2000/svg'><path fillRule='evenodd' d='M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z' clipRule='evenodd'></path></svg>
                 </div>
-                <input type="text" id="simple-search" className="bg-gray-50 border border-deviders text-gray-900 text-sm rounded-lg block w-full pr-7 p-2.5" placeholder="Search" required />
+                <input type='text' id='simple-search' className='bg-gray-50 border border-deviders text-gray-900 text-sm rounded-lg block w-full pr-7 p-2.5' placeholder='Search' required />
             </div>
             <div className='h-[46px] w-[138px] flex justify-center ml-2.5 bg-bcg_2 border border-deviders uppercase text-base rounded-[6px] px-2 py-[6px] flex items-center gap-1 cursor-pointer select-none'>
                 search
--- components/ButtonComponents/SelectButton.tsx
+++ components/ButtonComponents/SelectButton.tsx
@@ -16,8 +16,8 @@ export default function SelectButton({
 }: SelectProps) {
     return (
         <div className='w-full flex flex-col'>
-            <label className="block mb-2 text-sm font-sm text-dark">{category}</label>
-            <select id="countries" className="w-full bg-gray-50 border border-deviders text-dark text-sm rounded-lg block w-full p-2.5 h-[46px]">
+            <label className='block mb-2 text-sm font-sm text-dark'>{category}</label>
+            <select id='countries' className='w-full bg-gray-50 border border-deviders text-dark text-sm rounded-lg block w-full p-2.5 h-[46px]'>
                 <option defaultValue=''>Select&nbsp;{category}</option>
                 {selectItems.map((obj, index) => (
                     <option key={index} value={obj.value}>{obj.text}</option>
