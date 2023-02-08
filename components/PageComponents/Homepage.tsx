41941844f6c0b08d7803513a13cb5a2181fbd634
fpleader <alancameronboyd@gmail.com>
2023-02-07 20:08:52 -0800

    update

--- components/PageComponents/Homepage.tsx
+++ components/PageComponents/Homepage.tsx
@@ -4,7 +4,7 @@ export default function Homepage() {
     return (
         <div className='max-w-[1536px] m-auto min-h-screen pt-[90px]'>
             <div className='text-center text-[60px]'>
-                We are developing. please clieck the 'For parents/our teachers' or 'Blog/Articles'
+                We are developing. please clieck the ~For parents/our teachers~ or ~Blog/Articles~
             </div>
         </div>
     )
