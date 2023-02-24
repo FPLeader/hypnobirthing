8b1db7031d2b156ac1edc3fa96b18ea08b74b5c7
fpleader <alancameronboyd@gmail.com>
2023-02-24 14:04:20 -0800

    update

--- components/Cards/FeedbackCard.tsx
+++ components/Cards/FeedbackCard.tsx
@@ -1,4 +1,4 @@
-import ReactStars from 'react-rating-stars-component'
+// import ReactStars from '@types/react-star-rating-component'
 
 interface FeedbackCardProps {
   rating: number,
@@ -13,13 +13,13 @@ export default function FeedbackCard({
 }: FeedbackCardProps) {
   return (
     <div className='w-full flex flex-col items-center justify-center'>
-      <ReactStars
+      {/* <ReactStars
         count={5}
         size={32}
         edit={false}
         fullIcon={<i className='fa fa-star'></i>}
         activeColor='beighe'
-      />
+      /> */}
       <div className='mt-[15px] flex flex-col items-center gap-y-[10px] text-dark text-center'>
         <div className='text-[20px]'>{title}</div>
         <div className='w-0 h-[15px] border-r-[1px] border-dark'></div>
