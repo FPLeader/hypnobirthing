3362631fce85eaf5ab9cced0fb748b81abed17a1
fpleader <alancameronboyd@gmail.com>
2023-02-27 20:56:48 -0800

    update

--- components/Buttons/Normal.tsx
+++ components/Buttons/Normal.tsx
@@ -6,7 +6,7 @@ export default function NormalButton({
     text,
 }: ButtonProps) {
     return (
-        <div className='w-full md:w-max h-max text-center px-[38px] py-[10px] text-dark text-[14px] font-medium uppercase bg-beighe hover:bg-gray_1 active:bg-beighe rounded-[500px] select-none cursor-pointer transaction-all duration-300'>
+        <div className='w-full md:w-max h-max text-center px-[28px] lg:px-[38px] py-[6px] md:py-[8px] lg:py-[10px] text-dark text-[14px] font-medium uppercase bg-beighe hover:bg-gray_1 active:bg-beighe rounded-[500px] select-none cursor-pointer transaction-all duration-300'>
             {text}
         </div>
     )
--- components/Cards/Media.tsx
+++ components/Cards/Media.tsx
@@ -15,11 +15,11 @@ export default function Media({
 }: MediaCardProps) {
     return (
         <div className='w-full flex flex-col md:flex-row justify-between items-center gap-[20px] md:gap-[25px] border-[2px] border-beighe rounded-[20px] overflow-hidden'>
-            <div className='w-full md:max-w-[435px]'>
+            <div className='w-full md:max-w-[440px]'>
                 <img src={image} alt={title} className='h-auto w-full' />
             </div>
-            <div className='w-full flex flex-col gap-[15px] px-[20px] pb-[20px] md:pb-0 md:px-0 md:pr-[20px] lg:pr-[35px]'>
-                <div className='flex flex-col gap-[6px] text-dark text-center md:text-left'>
+            <div className='w-full flex flex-col gap-[10px] lg:gap-[15px] px-[20px] pb-[20px] md:pb-0 md:px-0 md:pr-[20px] lg:pr-[35px]'>
+                <div className='flex flex-col gap-[3px] lg:gap-[6px] text-dark text-center md:text-left'>
                     <div className='text-[14px] lg:text-[18px] opacity-60'>—&nbsp;{url}</div>
                     <div className='text-[18px] lg:text-[24px] font-semibold'>{title}</div>
                     <div className='text-[14px] lg:text-[18px] line-clamp-3'>{content}</div>
