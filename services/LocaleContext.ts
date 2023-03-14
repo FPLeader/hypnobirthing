921d1fe2896fbc24ef6ac218b87ebdb1a40c693a
fpleader <alancameronboyd@gmail.com>
2023-03-13 23:57:13 -0700

    Update LocaleContext.ts

--- services/LocaleContext.ts
+++ services/LocaleContext.ts
@@ -1,8 +1,8 @@
-import React from "react";
+import {createContext} from 'react';
 
 
 const defaultValue = {
   locale: 'en',
 }
 
-export default React.createContext(defaultValue);
+export default createContext(defaultValue);
