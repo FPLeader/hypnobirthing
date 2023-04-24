c58e5980fc4a4b5465b8b1f87845956878dfdd77
fpleader <alancameronboyd@gmail.com>
2023-04-24 11:20:28 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   env: {
-    DEV_MODE: true,
+    DEV_MODE: false,
     NEXT_PUBLIC_RECAPTCHA_SITE_KEY: '6LdQJR8lAAAAAKwEKMo297y-0-ERWeEgZhtmG-cR',
     API_BASE: process.env.DEV_MODE === true ?
       'http://localhost:5000/api/v1'
