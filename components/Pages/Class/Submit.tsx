adcd6a2b1cbb2c0f4a61e94a5a2a2a9adbafb969
fpleader <alancameronboyd@gmail.com>
2023-04-24 01:38:46 -0700

    update

--- components/Pages/Class/Submit.tsx
+++ components/Pages/Class/Submit.tsx
@@ -1,19 +1,53 @@
-import { CategoryInput, Textarea } from '@/components/Inputs'
+import { useState } from 'react';
+import { CategoryInput, TextareaNormal } from '@/components/Inputs'
 import { RegularButton } from '@/components/Buttons'
 
-export default function Submit() {
+interface SubmitProps {
+  lngId: number
+}
+
+export default function Submit({
+  lngId
+}: SubmitProps) {
+  // contact form
+  const [name, setName] = useState<string>('');
+  const [number, setNumber] = useState<string>('');
+  const [email, setEmail] = useState<string>('');
+  const [comments, setComments] = useState<string>('');
+
   return (
     <div>
       <div className='bg-bcg_2 p-[20px] lg:p-[30px] rounded-[10px] flex flex-col gap-[20px]'>
         <div className='text-center md:text-left text-[24px] lg:text-[28px] font-medium'>Enrollment is needed to participate in the event</div>
         <div className='flex flex-col gap-[10px]'>
           <div className='grid md:grid-cols-2 gap-[10px]'>
-            <CategoryInput category='Full name in Hebrew' placeholder='Enter your name' />
-            <CategoryInput category='Full name in English' placeholder='Enter your name' />
-            <CategoryInput category='Phone number' type='number' placeholder='+972' />
-            <CategoryInput category='Email' placeholder='user@example.com' />
+            <CategoryInput
+              category='Full name in English'
+              placeholder='Enter your name'
+              inputValue={name}
+              handleChange={setName}
+            />
+            <CategoryInput
+              category='Phone number'
+              type='number'
+              placeholder='+972'
+              inputValue={number}
+              handleChange={setNumber}
+            />
+            <CategoryInput
+              category='Email'
+              placeholder='user@example.com'
+              inputValue={email}
+              handleChange={setEmail}
+            />
           </div>
-          <Textarea category='Comments' placeholder='Enter text here' />
+          <TextareaNormal
+            category='Comments'
+            placeholder='Enter text here'
+            inputValue={comments}
+            handleChange={setComments}
+            lngId={lngId}
+          />
         </div>
         <div className='flex flex-col md:flex-row justify-between items-center gap-[20px]'>
           <RegularButton text='submit' />
--- components/Pages/Class/index.tsx
+++ components/Pages/Class/index.tsx
@@ -9,7 +9,9 @@ export default function Index() {
         <div className='w-full max-w-[1225px] mx-[20px]'>
           <div className='min-[1260px]:max-w-[805px] lg:max-w-[calc(100vw-460px)] w-full max-md:mt-[520px] max-lg:mt-[240px] flex flex-col gap-[20px] md:gap-[30px] lg:gap-[70px]'>
             <AboutClassSection />
-            <SubmitSection />
+            <SubmitSection
+              lngId={0}
+            />
             <FaqSection />
             <RelatedCoursesSection />
             <MyContactsSection />
