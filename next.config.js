d311d2186910a9ab5dee2818b00608d0edbe0656
fpleader <alancameronboyd@gmail.com>
2023-04-30 09:47:26 -0700

    update

--- next.config.js
+++ next.config.js
@@ -5,9 +5,9 @@ const nextConfig = {
   env: {
     DEV_MODE: true,
     NEXT_PUBLIC_RECAPTCHA_SITE_KEY: '6LdQJR8lAAAAAKwEKMo297y-0-ERWeEgZhtmG-cR',
-    // API_BASE: process.env.DEV_MODE === 'true' ? 'http://localhost:5000/api/v1' : 'https://45d0-135-181-13-63.ngrok-free.app/api/v1',
-    // FILE_IMAGE_BASE: process.env.DEV_MODE === 'true' ? 'http://localhost:5000/images/' : 'https://45d0-135-181-13-63.ngrok-free.app/images/',
-    // FILE_VIDEO_BASE: process.env.DEV_MODE === 'true' ? 'http://localhost:5000/videos/' : 'https://45d0-135-181-13-63.ngrok-free.app/videos/',
+    // API_BASE: process.env.DEV_MODE === 'true' ? 'http://localhost:5000/api/v1' : 'https://3f2f-135-181-13-63.ngrok-free.app/api/v1',
+    // FILE_IMAGE_BASE: process.env.DEV_MODE === 'true' ? 'http://localhost:5000/images/' : 'https://3f2f-135-181-13-63.ngrok-free.app/images/',
+    // FILE_VIDEO_BASE: process.env.DEV_MODE === 'true' ? 'http://localhost:5000/videos/' : 'https://3f2f-135-181-13-63.ngrok-free.app/videos/',
     API_BASE: 'http://localhost:5000/api/v1',
     FILE_IMAGE_BASE: 'http://localhost:5000/images/',
     FILE_VIDEO_BASE: 'http://localhost:5000/videos/',
