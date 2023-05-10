6912e481fa04b87fe4867f92f5507bac10acd71b
fpleader <alancameronboyd@gmail.com>
2023-05-10 16:03:41 -0700

    Update aboutus.tsx

--- pages/aboutus.tsx
+++ pages/aboutus.tsx
@@ -1,6 +1,6 @@
-// import dynamic from 'next/dynamic'
-// const AboutUsPage = dynamic(() => import('@/components/Pages/AboutUs'), { ssr: false })
-import { AboutUsPage } from "@/components/Pages"
+import dynamic from 'next/dynamic'
+const AboutUsPage = dynamic(() => import('@/components/Pages/AboutUs'), { ssr: false })
+// import { AboutUsPage } from "@/components/Pages"
 
 export default function aboutus() {
     return (
