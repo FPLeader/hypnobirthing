f9dad44f201e64849e29a1816c3cc8f54c83e939
fpleader <alancameronboyd@gmail.com>
2023-05-10 15:45:39 -0700

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,10 +1,11 @@
 import { Html, Head, Main, NextScript } from 'next/document';
+import Script from 'next/script'
 
 export default function Document() {
   return (
     <Html lang='en'>
       <Head>
-        <script src='/firebase-messaging-sw.js' />
+        <Script src='/firebase-messaging-sw.js'></Script>
       </Head>
       <body>
         <Main />
