d724fba2eec36fe4a817e33d9a093d3902b93523
fpleader <alancameronboyd@gmail.com>
2023-05-17 00:48:32 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -6,9 +6,9 @@ const nextConfig = {
     DEV_MODE: false,
     NEXT_PUBLIC_RECAPTCHA_SITE_KEY: '6LdQJR8lAAAAAKwEKMo297y-0-ERWeEgZhtmG-cR',
     REACT_APP_GOOGLE_MAPS_API_KEY: 'AIzaSyBjp4DGW9LacYAyDBac_3tnpRRMt5gYcRQ',
-     API_BASE: 'https://king-prawn-app-ml6q9.ondigitalocean.app/api/v1',
-     FILE_IMAGE_BASE: 'https://king-prawn-app-ml6q9.ondigitalocean.app/images/',
-     FILE_VIDEO_BASE: 'https://king-prawn-app-ml6q9.ondigitalocean.app/videos/',
+     API_BASE: 'http://138.197.185.155:5000/api/v1',
+     FILE_IMAGE_BASE: 'http://138.197.185.155:5000/images/',
+     FILE_VIDEO_BASE: 'http://138.197.185.155:5000/videos/',
 
     // API_BASE: 'http://localhost:5000/api/v1',
     // FILE_IMAGE_BASE: 'http://localhost:5000/images/',
