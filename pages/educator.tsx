8a3eaed29c1e6ff05b4cd62e49c6756472a28d95
fpleader <alancameronboyd@gmail.com>
2023-05-25 09:53:26 +0200

    Delete educator.tsx

--- pages/educator.tsx
+++
@@ -1,8 +0,0 @@
-import dynamic from 'next/dynamic'
-const EducatorPage = dynamic(() => import('@/components/Pages/Educator'), { ssr: false })
-
-export default function Support() {
-    return (
-        <EducatorPage />
-    )
-}
